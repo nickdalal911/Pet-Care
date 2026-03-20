9c6e2347415c9fe1a1139e57c2763cc16244e37f
Lokesh <yadavlokesh6572@gmail.com>
2026-03-20 14:14:00 +0530

    fix gemini model version

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -18,7 +18,8 @@ router.post("/", async (req, res) => {
     }
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash",
+      model: "gemini-1.5-flash-latest"
+      // model: "gemini-1.5-flash",
     });
 
     // const result = await model.generateContent(message);
